eb45ab73273202a3c2a5d04028b5f54c408d9994
Duane Terrazas <srtraz@gmail.com>
2014-11-23 23:27:50 -0600

    Changes for first challenge exercise

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +28,33 @@ class ViewController: UIViewController {
 
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        var nameSet = false
+        nameLabel.hidden = false
+        if !enterNameTextField.text.isEmpty {
+            nameLabel.text = enterNameTextField.text
+            nameLabel.textColor = UIColor.blueColor()
+            enterNameTextField.text = ""
+            enterNameTextField.resignFirstResponder()
+            nameSet = true
+        }
+        else {
+            nameLabel.text = "No Name Entered"
+        }
+        
+        var messageSet = false
         messageLabel.hidden = false
         if !enterMessageTextField.text.isEmpty {
             messageLabel.text = enterMessageTextField.text
             enterMessageTextField.text = ""
             enterMessageTextField.resignFirstResponder()
+            messageSet = true
         }
         else {
             messageLabel.text = "No Message Entered"
+        }
+        
+        if nameSet && messageSet == true {
+            mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         }
     }
     
